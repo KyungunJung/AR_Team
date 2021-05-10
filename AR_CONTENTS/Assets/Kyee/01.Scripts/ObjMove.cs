ab572fae0ebb8942bc68e0e1a19f9ce8275cdd89
hyungikimcreate <62112197+hyungikimcreate@users.noreply.github.com>
2021-05-10 09:17:46 +0900

    210510
    re

--- AR_CONTENTS/Assets/Kyee/01.Scripts/ObjMove.cs
+++ AR_CONTENTS/Assets/Kyee/01.Scripts/ObjMove.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ObjMove : MonoBehaviour
 {
-    //public Transform trChair;
-    public Transform[] models;
+    public Transform trChair;
+    //public Transform[] models;
     private void Update()
     {
         // 레이가 맞추고 있는 오브젝트의 정보를 담아오기 (수정사항)
@@ -17,25 +17,25 @@ public class ObjMove : MonoBehaviour
             int layer = 1 << LayerMask.NameToLayer("Grid");
             if (Physics.Raycast(ray, out hit, 100, layer))
             {
-                //trChair.position = hit.transform.position;
+                trChair.position = hit.transform.position;
 
-                for (int i=0; i < models.Length; i++)
-                {
-                    models[i].transform.position = hit.transform.position;
-                    // 모델의 정보는 한 개만 담는다.
-                }
+                //for (int i=0; i < models.Length; i++)
+                //{
+                //    models[i].transform.position = hit.transform.position;
+                //    // 모델의 정보는 한 개만 담는다.
+                //}
             }
         }
         
         // 왼쪽 마우스 버튼을 한번 더 클릭하면 오브젝트 회전 (수정사항)
         if(Input.GetMouseButtonDown(1))
         {
-            //trChair.transform.Rotate(new Vector3(0, 45, 0));
+            trChair.transform.Rotate(new Vector3(0, 45, 0));
 
-            for (int i = 0; i < models.Length; i++)
-            {
-                models[i].transform.Rotate(new Vector3(0, 45, 0));
-            }
+            //for (int i = 0; i < models.Length; i++)
+            //{
+            //    models[i].transform.Rotate(new Vector3(0, 45, 0));
+            //}
 
         }
     }
